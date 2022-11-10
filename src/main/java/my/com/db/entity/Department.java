c3efe5dbf23b54014f1c34cc8dc6791fe55c393b
Volodymyr Kyryliuk <kirilyukvolodimir2003@gmail.com>
2022-11-10 23:07:56 +0200

    initial commit

---
+++ src/main/java/my/com/db/entity/Department.java
@@ -0,0 +1,62 @@
+package my.com.db.entity;
+
+import java.util.Objects;
+
+public class Department {
+
+	private int id;
+
+	private String name;
+
+	public Department(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public static Department createDepartment(String name){
+		return new Department(0, name);
+	}
+
+	public void setName(String name) {
+//		try {
+//			Team team = DBManager.getInstance().getTeam(this.getName());
+//			this.id = team.id;
+//		} catch (DBException e) {
+//			throw new RuntimeException(e);
+//		}
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public static Department createTeam(String name){
+		return new Department(0, name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Department team = (Department) o;
+		return Objects.equals(name, team.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+	
+}
